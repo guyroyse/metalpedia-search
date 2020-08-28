f01cf6cd7325d616f4d492279d9512b1ef3a1505
Guy Royse <guy@guyroyse.com>
2020-08-28 16:43:43 -0400

    index songs

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const EventStream = require('./lib/event-stream')
 const EventHandler = require('./lib/event-handler')
+const SongIndexer = require('./lib/song-indexer')
 
 async function main() {
 
-  let eventStream = new EventStream()
-  let eventHandler = new EventHandler(eventStream)
+  let indexer, eventStream, eventHandler
 
   try {
+
+    indexer = new SongIndexer()
+    await indexer.start()
+
+    eventStream = new EventStream()  
     await eventStream.createGroup()
+
+    eventHandler = new EventHandler(eventStream, indexer)
     await eventHandler.start()
+
   } catch (error) {
     console.log(error)
+  } finally {
+    await indexer.quit()
+    await eventStream.quit()
   }
-
-  eventStream.quit()
 
 }
 
